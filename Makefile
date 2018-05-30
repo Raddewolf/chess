43372a93ea76e95546c76d43236c5ca91cb47e4b
Raddewolf <mrslimegood@gmail.com>
2018-05-30 12:04:04 +0700

    Add test

--- Makefile
+++ Makefile
@@ -6,17 +6,25 @@ FLAG = -Wall -Werror -std=c99
 
 default: bin/chess
 
+test: bin/chess-test
+
 bin/chess: build/main.o build/board.o build/function.o bin
 		$(CC) $(FLAG) build/main.o build/board.o build/function.o -o bin/chess
 
 build/main.o: src/main.c src/board.h src/function.h build
 		$(CC) $(FLAG) -c src/main.c -o build/main.o
 
-build/board.o: src/board.c src/function.h src/board.h build
+build/board.o: src/board.c src/function.h src/board.h build -lm
 		$(CC) $(FLAG) -c src/board.c -o build/board.o
 
 build/function.o: src/function.c src/function.h src/board.h build
 		$(CC) $(FLAG) -c src/function.c -o build/function.o
+
+bin/chess-test: build/main_test.o build/board.o build/function.o src bin
+		$(CC) $(FLAG) build/main_test.o build/board.o build/function.o -o bin/chess-test
+
+build/main_test.o: test/main.c thirdparty/ctest.h src/board.h src/function.h build
+		$(CC) $(FLAG) -I thirdparty -I src -c test/main.c -o build/main_test.o
 
 build:
 		mkdir build
---
+++ test/main.c
@@ -0,0 +1,101 @@
+#define CTEST_MAIN
+
+#include <function.h>
+#include <board.h>
+
+#include <ctest.h>
+#include <string.h>
+
+CTEST(Syntax, CorrectSyntaxCheck)
+{
+	int result = check_syntax("e2-e4");
+	int expected = 1;
+	ASSERT_EQUAL(expected,result);
+}
+
+CTEST(Syntax, IncorrectSyntaxCheck)
+{
+	int result = check_syntax("k9q64");
+	int expected = false;
+	ASSERT_EQUAL(expected,result);
+}
+
+CTEST(MovePawn0, Correct)
+{
+	log_t st;
+	strcpy(st.line, "e2-e4");
+	int result = make_move(&st);
+	int expected = 1;
+	ASSERT_EQUAL(expected,result);
+}
+
+CTEST(MovePawn1, Correct)
+{
+        log_t st;
+        strcpy(st.line, "e7-e5");
+        int result = make_move(&st);
+        int expected = 1;
+        ASSERT_EQUAL(expected,result);
+}
+
+CTEST(KillPawn, Correct)
+{
+        log_t st;
+        strcpy(st.line, "e4xe5");
+        int result = make_kill(&st);
+        int expected = 1;
+        ASSERT_EQUAL(expected,result);
+}
+
+CTEST(MovePawn2, Incorrect)
+{
+        log_t st;
+        strcpy(st.line, "e5-e4");
+        int result = make_move(&st);
+        int expected = 0;
+        ASSERT_EQUAL(expected,result);
+}
+
+CTEST(MovePawn3, Incorrect)
+{
+        log_t st;
+	strcpy(st.line, "e5-e7");
+        int result = make_move(&st);
+        int expected = 0;
+        ASSERT_EQUAL(expected,result);
+}
+
+CTEST(MoveKnight, Correct)
+{
+        log_t st;
+	strcpy(st.line, "b1-c3");
+        int result = make_move(&st);
+        int expected = 1;
+        ASSERT_EQUAL(expected,result);
+}
+
+CTEST(MoveBishop, Correct)
+{
+        log_t st;
+	strcpy(st.line, "f8-c5");
+        int result = make_move(&st);
+        int expected = 1;
+        ASSERT_EQUAL(expected,result);
+}
+
+CTEST(MoveRook, Correct)
+{
+        log_t st;
+	strcpy(st.line, "h1-h6");
+        int result = make_move(&st);
+        int expected = 1;
+        ASSERT_EQUAL(expected,result);
+}
+
+int main(int argc, const char** argv)
+{
+	init_board();
+	int test_res = ctest_main(argc, argv);
+	clean_board();
+	return test_res;
+}
